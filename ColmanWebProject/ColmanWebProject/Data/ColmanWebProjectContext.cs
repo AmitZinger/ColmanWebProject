38323e22d26eb874b5ffcded6e96d927baf9f9a0
Keren <kerenbonfil@gmail.com>
2021-05-17 16:44:33 +0300

    create productsController

---
+++ ColmanWebProject/ColmanWebProject/Data/ColmanWebProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ColmanWebProject.Models;
+
+namespace ColmanWebProject.Data
+{
+    public class ColmanWebProjectContext : DbContext
+    {
+        public ColmanWebProjectContext (DbContextOptions<ColmanWebProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ColmanWebProject.Models.Product> Product { get; set; }
+    }
+}
