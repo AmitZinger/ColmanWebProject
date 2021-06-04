fc0fe5429218fe4a254d959154d5d2f0b07fdb19
Shoval <shovalelyb@gmail.com>
2021-06-04 14:36:21 +0300

    Add style for delete customer page

--- ColmanWebProject/ColmanWebProject/Startup.cs
+++ ColmanWebProject/ColmanWebProject/Startup.cs
@@ -64,7 +64,7 @@ namespace ColmanWebProject
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Customers}/{action=Index}/{id?}");
             });
         }
     }
