71087b1b2f6c8efd9b02228e400103ecf5c34aea
Keren <kerenbonfil@gmail.com>
2021-06-11 13:15:45 +0300

    start creating popup to create categories

---
+++ ColmanWebProject/ColmanWebProject/Controllers/CategoriesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ColmanWebProject.Data;
+using ColmanWebProject.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ColmanWebProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly ColmanWebProjectContext _context;
+
+        public CategoriesController(ColmanWebProjectContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Category.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        
+        public IActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Type,SubType,Description")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                var checkExist = _context.Category.FirstOrDefault
+                (cat =>
+                    cat.Type.Equals(category.Type) && cat.SubType.Equals(category.SubType)
+                );
+
+                if (checkExist == null)
+                {
+                    _context.Add(category);
+                    await _context.SaveChangesAsync();
+                }
+                else
+                {
+                    ViewData["Error"] = "Catagory already exist; You can't create it again.";
+                }
+            }
+
+            return PartialView("CategoriesList", await _context.Category.ToListAsync());
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Type,SubType,Description")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Category.FindAsync(id);
+            _context.Category.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Category.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ColmanWebProject/ColmanWebProject/wwwroot/js/Popup.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+
+    $("#addBtn").click(function () {
+        $.ajax({
+            url: $(this).attr("formaction"),
+        }).done(function (msg) {
+            $("#AddContent").html(msg);
+            $("#add-contact").modal("show");
+        })
+    });
+
+    $("body").on("click", "#save", function () {
+        var form = $('form');
+        var token = $('input[name="__RequestVerificationToken"]', form).val();
+        $.ajax({
+            type: "post",
+            url: "/Categories/Create",
+            data: {
+                __RequestVerificationToken: token,
+                category: {
+                    Type: $("#Type").val(),
+                    SubType: $("#SubType").val(),
+                    Description: $("#Description").val()
+                }
+            },
+            dataType: "html",
+            success: function (result) {
+                $("#add-contact").modal("hide");
+                $("#partial").html(result);
+            }
+        });
+        return false;
+    });
+})
