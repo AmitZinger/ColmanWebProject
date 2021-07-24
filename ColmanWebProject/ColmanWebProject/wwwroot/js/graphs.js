c9fdfbd345e343a71d761dec904320908cc60f4c
Amit Zinger <amit@zinger.ws>
2021-07-24 13:15:01 +0300

    base products graph, only view without connection to db

--- ColmanWebProject/ColmanWebProject/wwwroot/js/graphs.js
+++ ColmanWebProject/ColmanWebProject/wwwroot/js/graphs.js
@@ -1,59 +1,156 @@
 ﻿$(document).ready(function () {
-    // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 40 },
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+    const sample = [
+        {
+            name: 'Guitar',
+            value: 8
+        },
+        {
+            name: 'Drums',
+            value: 5
+        },
+        {
+            name: 'Piano',
+            value: 6
+        }
+    ];
 
-    // append the svg object to the body of the page
-    var svg = d3.select("#graph")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+    const svg = d3.select('#container').append("svg");
 
-    // get the data
-    d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/1_OneNum.csv", function (data) {
+    const margin = 80;
+    const width = 1000 - 2 * margin;
+    const height = 600 - 2 * margin;
 
-        // X axis: scale and draw:
-        var x = d3.scaleLinear()
-            .domain([0, 1000])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
-            .range([0, width]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+    const chart = svg.append('g')
+        .attr('transform', `translate(${margin}, ${margin})`);
 
-        // set the parameters for the histogram
-        var histogram = d3.histogram()
-            .value(function (d) { return d.price; })   // I need to give the vector of value
-            .domain(x.domain())  // then the domain of the graphic
-            .thresholds(x.ticks(70)); // then the numbers of bins
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(sample.map((s) => s.name))
+        .padding(0.4)
 
-        // And apply this function to data to get the bins
-        var bins = histogram(data);
+    const values = sample.map(a => a.value);
+    const yDomainMax = Math.max(...values) + Math.min(...values);
 
-        // Y axis: scale and draw:
-        var y = d3.scaleLinear()
-            .range([height, 0]);
-        y.domain([0, d3.max(bins, function (d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-        svg.append("g")
-            .call(d3.axisLeft(y));
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, yDomainMax]);
 
-        // append the bar rectangles to the svg element
-        svg.selectAll("rect")
-            .data(bins)
-            .enter()
-            .append("rect")
-            .attr("x", 1)
-            .attr("transform", function (d) {
-                console.log(d);
-                debugger
-                return "translate(" + x(d.x0) + "," + y(d.length) + ")";
-            })
-            .attr("width", function (d) { return x(d.x1) - x(d.x0) - 1; })
-            .attr("height", function (d) { return height - y(d.length); })
-            .style("fill", "#69b3a2")
+    const makeYLines = () => d3.axisLeft()
+        .scale(yScale)
 
-    });
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+    chart.append('g')
+        .call(d3.axisLeft(yScale));
+
+    chart.append('g')
+        .attr('class', 'grid')
+        .call(makeYLines()
+            .tickSize(-width, 0, 0)
+            .tickFormat('')
+        )
+
+    const barGroups = chart.selectAll()
+        .data(sample)
+        .enter()
+        .append('g')
+
+    barGroups
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('x', (g) => xScale(g.name))
+        .attr('y', (g) => yScale(g.value))
+        .attr('height', (g) => height - yScale(g.value))
+        .attr('width', xScale.bandwidth())
+        .on('mouseenter', function (actual, i) {
+            d3.selectAll('.value')
+                .attr('opacity', 0)
+
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr('opacity', 0.6)
+                .attr('x', (a) => xScale(a.name) - 5)
+                .attr('width', xScale.bandwidth() + 10)
+
+            const y = yScale(actual.value)
+
+            line = chart.append('line')
+                .attr('id', 'limit')
+                .attr('x1', 0)
+                .attr('y1', y)
+                .attr('x2', width)
+                .attr('y2', y)
+
+            barGroups.append('text')
+                .attr('class', 'divergence')
+                .attr('x', (a) => xScale(a.name) + xScale.bandwidth() / 2)
+                .attr('y', (a) => yScale(a.value) + 30)
+                .attr('fill', 'white')
+                .attr('text-anchor', 'middle')
+                .text((a, idx) => {
+                    const divergence = (a.value - actual.value).toFixed(1)
+
+                    let text = ''
+                    if (divergence > 0) text += '+'
+                    text += `${divergence}`
+
+                    return idx !== i ? text : '';
+                })
+
+        })
+        .on('mouseleave', function () {
+            d3.selectAll('.value')
+                .attr('opacity', 1)
+
+            d3.select(this)
+                .transition()
+                .duration(300)
+                .attr('opacity', 1)
+                .attr('x', (a) => xScale(a.name))
+                .attr('width', xScale.bandwidth())
+
+            chart.selectAll('#limit').remove()
+            chart.selectAll('.divergence').remove()
+        })
+
+    barGroups
+        .append('text')
+        .attr('class', 'value')
+        .attr('x', (a) => xScale(a.name) + xScale.bandwidth() / 2)
+        .attr('y', (a) => yScale(a.value) + 30)
+        .attr('text-anchor', 'middle')
+        .text((a) => `${a.value}`)
+
+    svg
+        .append('text')
+        .attr('class', 'label')
+        .attr('x', -(height / 2) - margin)
+        .attr('y', margin / 2.4)
+        .attr('transform', 'rotate(-90)')
+        .attr('text-anchor', 'middle')
+        .text('Orders')
+
+    svg.append('text')
+        .attr('class', 'label')
+        .attr('x', width / 2 + margin)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'middle')
+        .text('Products')
+
+    svg.append('text')
+        .attr('class', 'title')
+        .attr('x', width / 2 + margin)
+        .attr('y', 40)
+        .attr('text-anchor', 'middle')
+        .text('Most Ordered Products')
+
+    svg.append('text')
+        .attr('class', 'source')
+        .attr('x', width - margin / 2)
+        .attr('y', height + margin * 1.7)
+        .attr('text-anchor', 'start')
+        .text('Per: ' + new Date().toLocaleDateString())
 })
